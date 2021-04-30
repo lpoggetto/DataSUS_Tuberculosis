8dfa35494df2b7651aee15c1b603ccc4f3e6f1d4
Lucas Ribeiro Del Poggetto <15705695+lpoggetto@users.noreply.github.com>
2021-04-29 23:25:49 -0300

    Finished fixing data and created a simple EDA

--- DataSUS_TB.R
+++ DataSUS_TB.R
@@ -1,5 +1,10 @@
 library(tidyverse)
 library(varhandle)
+library(read.dbc)
+library(lubridate)
+library(ggmap)
+library(treemap)
+library(networkD3)
 
 setwd("~/R/DataSUS/Dados")
 
@@ -67,7 +72,10 @@ dados_teste <- dados_teste %>%
                              '4' = 'Parda',
                              '5' = 'Indigena',
                              '9' = 'Ignorado',
-                             '0' = 'Ignorado'))
+                             '0' = 'Ignorado',
+                             'A' = 'Amarela',
+                             'N' = 'Preta',
+                             'B' = 'Branca'))
 
 # Atribuindo niveis de escolaridade SUS
 
@@ -75,15 +83,25 @@ dados_teste <- dados_teste %>%
      mutate(CS_ESCOL_N = recode(CS_ESCOL_N,
                                 '0' = 'Analfabeto',
                                 '1' = 'EF_Incompleto',
+                                '01' = 'EF_Incompleto',
                                 '2' = 'EF_Incompleto',
+                                '02' = 'EF_Incompleto',
                                 '3' = 'EF_Incompleto',
+                                '03' = 'EF_Incompleto',
                                 '4' = 'EF_Completo',
+                                '04' = 'EF_Completo',
                                 '5' = 'EM_Incompleto',
+                                '05' = 'EM_Incompleto',
                                 '6' = 'EM_Completo',
+                                '06' = 'EM_Completo',
                                 '7' = 'ES_Incompleto',
+                                '07' = 'ES_Incompleto',
                                 '8' = 'ES_Completo',
+                                '08' = 'ES_Completo',
                                 '9' = 'Ignorado',
-                                '10' = 'N_aplicavel'))
+                                '09' = 'Ignorado',
+                                '10' = 'N_aplicavel',
+                                '00' = 'Ignorado'))
 
 # Tipo de tratamento inicial
 
@@ -171,14 +189,23 @@ dados_teste <- dados_teste %>%
 dados_teste <- dados_teste %>% 
      mutate(SITUA_ENCE = recode(SITUA_ENCE,
                                 '1' = 'Cura',
+                                '01' = 'Cura',
                                 '2' = 'Abandono',
+                                '02' = 'Abandono',
                                 '3' = 'Obito_Tuberculose',
+                                '03' = 'Obito_Tuberculose',
                                 '4' = 'Obito_outras',
+                                '04' = 'Obito_outras',
                                 '5' = 'Transferencia',
+                                '05' = 'Transferencia',
                                 '6' = 'Mud_Diagnostico',
+                                '06' = 'Mud_Diagnostico',
                                 '7' = 'TB_DR',
+                                '07' = 'TB_DR',
                                 '8' = 'Mud_Esquema',
+                                '08' = 'Mud_Esquema',
                                 '9' = 'Falencia',
+                                '09' = 'Falencia',
                                 '10' = 'Abandono_primario'))
 
 # Categorias Tipo Populacional
@@ -188,24 +215,28 @@ dados_teste <- dados_teste %>%
      mutate(POP_LIBER = recode(POP_LIBER,
                                '1' = 'Sim',
                                '2' = 'Nao',
+                               '3' = 'Ignorado',
                                '9' = 'Ignorado'))
 
 dados_teste <- dados_teste %>%
      mutate(POP_RUA = recode(POP_RUA,
                              '1' = 'Sim',
                              '2' = 'Nao',
+                             '3' = 'Ignorado',
                              '9' = 'Ignorado'))
 
 dados_teste <- dados_teste %>%
      mutate(POP_SAUDE = recode(POP_SAUDE,
                                '1' = 'Sim',
                                '2' = 'Nao',
+                               '3' = 'Ignorado',
                                '9' = 'Ignorado'))
 
 dados_teste <- dados_teste %>%
      mutate(POP_IMIG = recode(POP_IMIG,
                               '1' = 'Sim',
                               '2' = 'Nao',
+                              '3' = 'Ignorado',
                               '9' = 'Ignorado'))
 
 # Categoria sexo
@@ -216,7 +247,7 @@ dados_teste <- dados_teste %>%
                              'F' = 'Feminino',
                              'I' = 'Ignorado'))
 
-#categoria transf
+# categoria transf
 
 dados_teste <- dados_teste %>% 
      mutate(TRANSF = recode(TRANSF,
@@ -225,6 +256,17 @@ dados_teste <- dados_teste %>%
                             '3' = 'UF_diferente',
                             '4' = 'Pais_diferente',
                             '9' = 'Ignorado'))
+
+# ajustando a coluna de idade
+
+# filtrando colunas que são maior que 4000
+dados_teste <- dados_teste %>% 
+        filter(NU_IDADE_N > 4000)
+
+# subtraindo 4000 do vetor para ter a idade real do paciente
+# ex 4022 - 4000 = 22 anos
+
+dados_teste$NU_IDADE_N <- dados_teste$NU_IDADE_N - 4000
 
 # tirando algumas outras colunas que julgava que seriam uteis
 # no fim não terão uso nenhum
@@ -243,3 +285,241 @@ dados_final <- select(dados_teste, colunas_finais)
 
 dados_final <- dados_final %>% left_join(municipios, by = 'ID_MUNICIP')
 
+# visualização de dados
+
+# visualizando a quantitadade de tratamentos por ano
+
+p1 <- dados_final %>% 
+        select(DT_INIC_TR) %>% 
+        na.omit() %>% 
+        group_by(DT_INIC_TR) %>%
+        summarise(frequencia = n()) %>% 
+        filter(DT_INIC_TR >= as.Date("2001-01-01") & DT_INIC_TR < as.Date("2017-01-01")) %>% 
+        mutate(ano = year(DT_INIC_TR),
+               mes = month(DT_INIC_TR),
+               mes_nome = as.factor(recode(mes,
+                                 '1' = 'Jan',
+                                 '2' = 'Fev',
+                                 '3' = 'Mar',
+                                 '4' = 'Abr',
+                                 '5' = 'Mai',
+                                 '6' = 'Jun',
+                                 '7' = 'Jul',
+                                 '8' = 'Ago',
+                                 '9' = 'Set',
+                                 '10' = 'Out',
+                                 '11' = 'Nov',
+                                 '12' = 'Dez')))
+
+p1_mes <- p1 %>% 
+        group_by(ano, mes, mes_nome) %>% 
+        summarise(total_casos = sum(frequencia))
+
+p1_mes$mes_nome <- fct_relevel(p1_mes$mes_nome, "Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+                    "Jul", "Ago", "Set", "Out", "Nov", "Dez")
+
+g1 <- ggplot(data = p1_mes, aes(x = mes, y = total_casos, fill = mes_nome)) +
+        geom_bar(stat = "identity") +
+        facet_wrap( ~ ano, ncol = 4) +
+        labs(title = "Total de casos de Tuberculose por Ano",
+             subtitle = "Estado de São Paulo",
+             y = 'quantidade de casos',
+             x = "Mês") + theme_bw(base_size = 15) +
+        theme(axis.title.x=element_blank(),
+              axis.text.x=element_blank(),
+              axis.ticks.x=element_blank()) +
+        guides(fill = guide_legend(title = "Meses"))
+
+g1 
+
+# Infectados - Sexo
+
+p2 <-  dados_final %>% 
+        select(NU_ANO, CS_SEXO) %>%
+        filter(CS_SEXO != "Ignorado") %>%
+        #filter(NU_ANO >= 2001 & NU_ANO <=2016) %>% 
+        group_by(NU_ANO, CS_SEXO) %>% 
+        summarise(freq = n())
+
+g2 <- ggplot(data = p2, aes(x = CS_SEXO, y = freq)) +
+        geom_bar(fill = 'steelblue', stat = 'identity') +
+        labs(title = "Total de casos por Sexo",
+             subtitle = "Estado de São Paulo",
+             y = 'quantidade de casos',
+             x = "")
+
+g2
+
+# boxplot - Idade
+
+p3 <- dados_final %>% 
+        select(NU_IDADE_N, CS_SEXO) %>% 
+        filter(CS_SEXO != "Ignorado")
+
+g3 <- ggplot(data = p3, aes(x = CS_SEXO, y = NU_IDADE_N, fill = CS_SEXO)) + 
+        geom_boxplot() +
+        labs(title = "Distribuição de idade por Sexo",
+             subtitle = "Estado de São Paulo",
+             y = 'Idade',
+             x = "") +
+        guides(fill = guide_legend(title = "Sexo"))
+
+g3
+
+# boxplot - raca
+
+p4 <- dados_final %>%
+        select(CS_RACA, NU_IDADE_N, CS_SEXO) %>% 
+        na.omit()
+
+g4 <- ggplot(data = p4, aes(x = CS_RACA, y = NU_IDADE_N, fill = CS_SEXO)) + 
+        geom_boxplot() +
+        labs(title = "Distribuição de idade por Raça e Sexo",
+             subtitle = "Estado de São Paulo",
+             y = 'Idade',
+             x = "")
+
+g4
+
+# boxplot - nivel escolar
+
+p5 <- dados_final %>%
+        select(CS_ESCOL_N, NU_IDADE_N, CS_SEXO) %>% 
+        na.omit()
+
+p5$CS_ESCOL_N <- fct_relevel(p5$CS_ESCOL_N,
+                             "Ignorado", "N_aplicavel", "Analfabeto",
+                             "EF_Incompleto", "EF_Completo", "EM_Incompleto",
+                             "EM_Completo", "ES_Incompleto", "ES_Completo")
+
+g5 <- ggplot(data = p5, aes(x = CS_ESCOL_N, y = NU_IDADE_N)) + 
+        geom_boxplot(fill = 'deeppink3') +
+        labs(title = "Distribuição de idade por Escolaridade",
+             subtitle = "Estado de São Paulo",
+             y = 'Idade',
+             x = "")
+
+g5
+
+# tratamento
+
+p6 <- dados_final %>% 
+        select(TRATAMENTO, CS_SEXO) %>% 
+        group_by(TRATAMENTO, CS_SEXO) %>% 
+        filter(CS_SEXO != "Ignorado") %>% 
+        summarise(freq = length(TRATAMENTO)) %>% 
+        na.omit()
+
+g6 <- ggplot(data = p6) +
+        geom_bar(mapping = aes(x = TRATAMENTO, y = freq, fill = CS_SEXO),
+                 stat = 'identity', position = position_dodge()) +
+        labs(title = "Total de casos novos por Sexo",
+             subtitle = "Estado de São Paulo",
+             y = 'quantidade de casos',
+             x = "")
+        
+g6
+
+# forma da tuberculose
+
+p7 <- dados_final %>% 
+        select(FORMA, CS_SEXO) %>% 
+        group_by(FORMA, CS_SEXO) %>% 
+        filter(CS_SEXO != "Ignorado") %>% 
+        summarise(freq = length(FORMA)) %>% 
+        na.omit()
+
+g7 <- ggplot(data = p7) +
+        geom_bar(mapping = aes(x = FORMA, y = freq, fill = CS_SEXO),
+                 stat = 'identity', position = position_dodge()) +
+        labs(title = "Tipo de tuberculose por Sexo",
+             subtitle = "Estado de São Paulo",
+             y = 'quantidade de casos',
+             x = "")
+
+
+g7
+
+# situacao encerramento
+
+p8 <- dados_final %>% 
+        select(SITUA_ENCE, CS_SEXO) %>% 
+        group_by(SITUA_ENCE, CS_SEXO) %>% 
+        filter(CS_SEXO != "Ignorado") %>%
+        summarise(freq = length(SITUA_ENCE)) %>% 
+        na.omit()
+
+g8 <- ggplot(data = p8) +
+        geom_bar(mapping = aes(x = SITUA_ENCE, y = freq, fill = CS_SEXO),
+                 stat = 'identity', position = position_dodge()) +
+        labs(title = "Situação de encerramento por Sexo",
+             subtitle = "Estado de São Paulo",
+             y = 'quantidade de casos',
+             x = "")
+
+
+g8
+
+# concentracao de casos de TB por microrregiao de SP
+
+p9 <- dados_final %>% 
+        filter(UF == 35 & TRATAMENTO == "Caso_Novo") %>% 
+        select(Nome_Microrregiao, TRATAMENTO) %>% 
+        group_by(Nome_Microrregiao, TRATAMENTO) %>%
+        summarise(freq = length(TRATAMENTO)) %>% 
+        na.omit()
+
+g9 <- treemap(p9,
+        index = "Nome_Microrregiao",
+        vSize = "freq",
+        type = "index",
+        fontsize.labels = 12,
+        title = "Concentração de novos casos de Tuberculose por microrregião de São Paulo")
+
+# para aonde vao as transferencias - Diagrama de Sankey
+
+p10 <- dados_final %>% 
+        select(Nome_UF, UF) %>% 
+        mutate(Origem = "São Paulo") %>% 
+        filter(UF != 35) %>% 
+        group_by(Nome_UF, UF, Origem) %>% 
+        summarise(freq = length(Nome_UF))
+
+# A connection data frame is a list of flows with intensity for each flow
+
+links <- data.frame(
+        source = p10$Origem,
+        target = p10$Nome_UF,
+        value = p10$freq)
+
+# From these flows we need to create a node data frame: it lists every entities involved in the flow
+
+nodes <- data.frame(
+        name=c(as.character(links$source),
+               as.character(links$target)) %>% 
+                unique())
+
+# With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
+
+links$IDsource <- match(links$source, nodes$name)-1
+links$IDtarget <- match(links$target, nodes$name)-1
+
+# Make the Network
+
+g10 <- sankeyNetwork(Links = links, Nodes = nodes,
+                     Source = "IDsource", Target = "IDtarget",
+                     Value = "value", NodeID = "name", 
+                     sinksRight=FALSE, fontSize = 15)
+
+g10
+
+g1
+g2
+g3
+g4
+g5
+g6
+g7
+g8
+g9
+g10
